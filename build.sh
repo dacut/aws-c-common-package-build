799b659584a34e4d61de630b9ea2c39d3908d3be
David Cuthbert <dacut@kanga.org>
2019-08-27 01:07:41 -0700

    Add Ubuntu builds. Target /srv/dist.ionosphere.io instead of local export directory.

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ PACKAGE=aws-c-common
 VERSION=0.4.3
 REL=0
 DOWNLOAD_URL="https://github.com/awslabs/aws-c-common/archive/v$VERSION.tar.gz"
-EXPORT=$(realpath $(dirname $0))/export
+EXPORT=/srv/dist.ionosphere.io
+
+umask 002
 
 if [[ ! -f v$VERSION.tar.gz ]]; then
     curl -s -o v$VERSION.tar.gz "$DOWNLOAD_URL"
@@ -12,8 +14,14 @@ fi
 
 for TARGET in amzn1 amzn2 el7; do
 	docker build -t build-${PACKAGE}:$TARGET -f $TARGET.dockerfile .
-	rm -rf $EXPORT/$TARGET
 	mkdir -p $EXPORT/$TARGET/SRPMS $EXPORT/$TARGET/RPMS/$ARCH
 	docker run --rm --volume $EXPORT/${TARGET}:/export build-${PACKAGE}:$TARGET \
 	  /bin/sh -c "cp /usr/src/rpm/SRPMS/* /export/SRPMS && cp /usr/src/rpm/RPMS/$ARCH/* /export/RPMS/$ARCH/"
+done;
+
+for TARGET in ubuntu-xenial ubuntu-bionic ubuntu-cosmic ubuntu-disco; do
+	docker build -t build-${PACKAGE}:$TARGET -f $TARGET.dockerfile .
+	mkdir -p $EXPORT/$TARGET
+		docker run --rm --volume $EXPORT/${TARGET}:/export build-${PACKAGE}:$TARGET \
+	  /bin/bash -c "shopt -s nullglob; cp -f /build/*.deb /build/*.dsc /build/*.changes /build/*.debian.tar.* /build/*.orig.tar.* /build/*.buildinfo /export/"
 done;
---
+++ ubuntu-bionic.dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:bionic
+RUN apt update -y
+
+# Install build dependencies
+RUN apt install -y binutils build-essential cmake debhelper dh-make gcc make
+RUN mkdir /build
+COPY v0.4.3.tar.gz /build/aws-c-common_0.4.3.orig.tar.gz
+WORKDIR /build
+RUN tar xf aws-c-common_0.4.3.orig.tar.gz
+RUN mv aws-c-common-0.4.3 aws-c-common_0.4.3-0
+COPY debian /build/aws-c-common_0.4.3-0/debian/
+WORKDIR /build/aws-c-common_0.4.3-0
+RUN dpkg-checkbuilddeps
+RUN dpkg-buildpackage
+VOLUME /export
